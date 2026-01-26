596ba03ef08cda42b5cd65aace33280d9d0b9c1c
Gayoung03 <gayoungk0312@gmail.com>
2026-01-26 23:54:11 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,12 @@
+def solution(a, b, c):
+    answer = 0
+    
+    count = len(set([a,b,c]))
+    
+    if count ==3:
+        answer = a+b+c
+    elif count ==2:
+        answer = (a+b+c) * (a**2+b**2+c**2)
+    else:
+         answer = (a+b+c) * (a**2+b**2+c**2) * (a**3+b**3+c**3)
+    return answer
